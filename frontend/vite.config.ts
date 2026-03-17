726220918d56d55aaa97167fa81bbeb94e6c232d
Conrad Bradford <124806031+conlad1@users.noreply.github.com>
2026-03-17 00:17:58 -0600

    test deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  const proxyTarget = env.VITE_PROXY_TARGET || process.env.VITE_PROXY_TARGET || 'http://localhost:5015'
+  const env = loadEnv(mode, process.cwd(), '');
+  const proxyTarget =
+    env.VITE_PROXY_TARGET ||
+    process.env.VITE_PROXY_TARGET ||
+    'http://is401-catering-env.eba-vyzubsvg.us-west-2.elasticbeanstalk.com/';
 
   return {
     plugins: [react()],
@@ -17,5 +20,5 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
-  }
-})
+  };
+});
